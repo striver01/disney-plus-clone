d18d7cf5ef6375adbcbed16b32e5ab749bcbad48
shubh-coder22 <shubh.purwar_cs19@gla.ac.in>
2022-01-14 20:59:23 +0530

    main carausel made

---
+++ src/Components/Viewers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function Viewers() {
+  return (
+    <div>
+      Viewers
+      <a></a>
+    </div>
+  );
+}
+
+export default Viewers;
