8242ceb1ec7388ceb7b9b76f09685c914679245e
shubh-coder22 <shubh.purwar_cs19@gla.ac.in>
2022-01-15 06:52:22 +0530

    login page completed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,83 +1,83 @@
-// import React from "react";
-// import styled from "styled-components";
+import React from "react";
+import styled from "styled-components";
 
-// function Login() {
-//   return (
-//     <Container>
-//       <Background>
-//         <img src="/images/login-background.jpg" />
-//       </Background>
-//       <ContentBox>
-//         <Logo src="/images/cta-logo-one.svg" />
-//         <SignUp>GET ALL THERE</SignUp>
-//         <Description>
-//           lorem ipsum fednhan iu uweh iuhreh odj ;o oihqoh daoishdohed
-//           ohd;kheuiofd herdio ahe;o qoi oqi oahfiuskjcbsuifgwkhdiuqwe oiuio
-//           hoqiehro;iehwf;sa oih sbdficbn hfo ;en jjaijsfkj iowefoi hwfio oiihoif
-//           hkansfdoohweioa knsdoi eoihfw
-//         </Description>
-//         <LoginLogo src="/images/cta-logo-two.png" />
-//       </ContentBox>
-//     </Container>
-//   );
-// }
+function Login() {
+  return (
+    <Container>
+      <Background>
+        <img src="/images/login-background.jpg" />
+      </Background>
+      <ContentBox>
+        <Logo src="/images/cta-logo-one.svg" />
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          lorem ipsum fednhan iu uweh iuhreh odj ;o oihqoh daoishdohed
+          ohd;kheuiofd herdio ahe;o qoi oqi oahfiuskjcbsuifgwkhdiuqwe oiuio
+          hoqiehro;iehwf;sa oih sbdficbn hfo ;en jjaijsfkj iowefoi hwfio oiihoif
+          hkansfdoohweioa knsdoi eoihfw
+        </Description>
+        <LoginLogo src="/images/cta-logo-two.png" />
+      </ContentBox>
+    </Container>
+  );
+}
 
-// export default Login;
+export default Login;
 
-// const Container = styled.div`
-//   height: calc(100vh-70px);
-// `;
+const Container = styled.div`
+  height: calc(100vh-70px);
+`;
 
-// const Background = styled.div`
-//   position: relative;
-//   top: 0;
-//   right: 0;
-//   bottom: 0;
-//   left: 0;
-//   img {
-//     position: fixed;
-//     width: 100%;
-//     z-index: -1;
-//     height: 100%;
-//     no-repeat: fixed;
-//     object-fit: center;
-//   }
-// `;
+const Background = styled.div`
+  position: relative;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  img {
+    position: fixed;
+    width: 100%;
+    z-index: -1;
+    height: 100%;
+    no-repeat: fixed;
+    object-fit: center;
+  }
+`;
 
-// const ContentBox = styled.div`
-//   max-width: 650px;
-//   padding: 80px 40px;
-//   margin: auto;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   flex-direction: column;
-// `;
+const ContentBox = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
-// const Logo = styled.img``;
+const Logo = styled.img``;
 
-// const SignUp = styled.a`
-//   background-color: #0063e5;
-//   width: 100%;
-//   font-weight: bold;
-//   padding: 17px 0;
-//   border-radius: 5px;
-//   text-align: center;
-//   color: #f9f9f9;
-//   cursor: pointer;
-//   transition: all 250ms;
-//   margin-top: 8px;
-//   letter-spacing: 1.5px;
-//   margin-bottom: 17px;
-//   &:hover {
-//     background: #0483ee;
-//   }
-// `;
+const SignUp = styled.a`
+  background-color: #0063e5;
+  width: 100%;
+  font-weight: bold;
+  padding: 17px 0;
+  border-radius: 5px;
+  text-align: center;
+  color: #f9f9f9;
+  cursor: pointer;
+  transition: all 250ms;
+  margin-top: 8px;
+  letter-spacing: 1.5px;
+  margin-bottom: 17px;
+  &:hover {
+    background: #0483ee;
+  }
+`;
 
-// const Description = styled.div``;
+const Description = styled.div``;
 
-// const LoginLogo = styled.img`
-//   width: 100%;
-//   height: 100%;
-//   margin-top: 10px;
-// `;
+const LoginLogo = styled.img`
+  width: 100%;
+  height: 100%;
+  margin-top: 10px;
+`;
